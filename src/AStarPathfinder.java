c93e8d460266b0c6f801a1cefa8b25fd7a70cc89
Nelson Sanchez <nelson@dev2games.com>
2018-07-14 04:04:49 -0400

    Initial Commit

---
+++ src/AStarPathfinder.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 
+ * @author Nelson Sanchez
+ *
+ */
+
+public class AStarPathfinder {
+	
+	//Represents the open set in the pathfinder
+	public ArrayList<Integer> openSet = new ArrayList<Integer>();
+	
+	//Represents the closed set in the pathfinder
+	public ArrayList<Integer> closedSet = new ArrayList<Integer>();
+	
+	public AStarPathfinder(Vector start, Vector goal) {
+		
+	}
+	
+}
---
+++ src/Vector.java
@@ -0,0 +1,41 @@
+/**
+ * Represents a point vector
+ * @author Nelson Sanchez
+ *
+ */
+
+public class Vector {
+	
+	int x;
+	int y;
+	
+	/**
+	 * Constructor
+	 * @param x the x position
+	 * @param y the y position
+	 */
+	
+	public Vector(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	/**
+	 * Gets the x position
+	 * @return the x position
+	 */
+	
+	public int getX() {
+		return x;
+	}
+	
+	/**
+	 * Gets the y position
+	 * @return the y position
+	 */
+	
+	public int getY() {
+		return y;
+	}
+
+}
